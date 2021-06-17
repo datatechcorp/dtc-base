dc73d70e9ca440503bd664079668915c24ed8c3a
h2plalbert <hohoangphilong@gmail.com>
2021-06-17 22:01:09 +0700

    fix: get account by address or create one

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -2,7 +2,7 @@ const { catchAsync } = require('../utils');
 const { accService } = require('../services');
 
 const getAccByAddress = catchAsync(async (req, res) => {
-  const account = await accService.getAccByAddress(req.query.account);
+  const account = await accService.createAcc({ address: req.query.address });
   res.send(account);
 });
 
